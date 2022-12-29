a20884e9bfeda22d113cc56c51e1343f7de1d16c
Mardav Jadaun <mardavjadaun113@gmail.com>
2022-12-29 21:09:14 +0530

    homepage completed with routing and slight change in all the webpages

--- untitled/src/App.js
+++ untitled/src/App.js
@@ -1,29 +1,29 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Sidebar from "./components/sidebar";
+import Sidebar from './components/sidebar';
 import Home from './webpages/Home';
 import Community from './webpages/Community';
+import Profile from './webpages/Profile';
 import Compiler from './webpages/Compiler';
 import About from './webpages/About';
-import Profile from './webpages/Profile';
 import Signup from './webpages/Signup';
+
 
 const App = () => {
   return (
     <>
-    <BrowserRouter>
-      <Sidebar>
+      <BrowserRouter>
+        <Sidebar/>
         <Routes>
-          <Route path="/" element={<Home/>} />
+          <Route path="/" element={<Home/>}/>
           <Route path="/home" element={<Home/>} />
-          <Route path="/community" element={<Community />} />
-          <Route path="/compiler" element={<Compiler />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/signup" element={<Signup />} />
+          <Route path="/community" element={<Community/>} />
+          <Route path="/compiler" element={<Compiler/>} />
+          <Route path="/about" element={<About/>} />
+          <Route path="/profile" element={<Profile/>} />
+          <Route path="/signup" element={<Signup/>} />
         </Routes>
-      </Sidebar>
-    </BrowserRouter>
+      </BrowserRouter>
     </>
   );
 };
--- untitled/src/components/sidebar.js
+++ untitled/src/components/sidebar.js
@@ -1,16 +1,15 @@
-import React, {useState} from 'react';
+import React from 'react';
 import '../App.css';
-import { HiOutlineHome, HiOutlineInformationCircle, HiOutlineMenuAlt4 } from 'react-icons/hi';
+import { HiOutlineHome, HiOutlineInformationCircle } from 'react-icons/hi';
+import { GiFlexibleStar } from 'react-icons/gi';
 import { IoIosPeople, } from 'react-icons/io';
 import { RiCodeSSlashLine, RiInstagramFill, RiLinkedinBoxFill, RiGithubFill, RiTwitterFill } from 'react-icons/ri';
 import { BsPerson, } from 'react-icons/bs';
 import { NavLink } from 'react-router-dom';
-import { MdPersonAdd } from 'react-icons/md'
 
 
-const Sidebar = ({children}) => {
-    const [isOpen, setIsOpen] = useState(false);
-    const toggle = () => setIsOpen(!isOpen);
+const Sidebar = () => {
+
     const menuItem=[
     {
         path: "/home",
@@ -41,42 +40,36 @@ const Sidebar = ({children}) => {
 
 const user=[
     {
-        way: "/signup",
-        call: "Log In / Sign Up ",
-        icon:<MdPersonAdd/>
-
+        path: "/signup",
+        name: "LogIn / SignUp "
     },
 ]
 
 return (
     <div className="flex w-full h-full fixed">
-        <div style={{width: isOpen ? "300px":"65px"}}className="bg-black text-white px-1 py-5 transition-0.5 duration-500">
-            <div className="flex items-center p-6">
-                <h1 style={{ display: isOpen ? "block" : "none" }} className="text-3xl text-center text-white ">lOiOl</h1>
-                    <div style={{ marginLeft: isOpen ? "100px" : "-6px" }} className="flex text-2xl cursor-pointer hover:text-semibold">
-                    <HiOutlineMenuAlt4 onClick={toggle}/>
-                </div>
+        <div className="bg-black text-white m-2 w-72">
+            <div className="flex p-10 ">
+                <GiFlexibleStar size={45}/>
             </div>  
             {
                 menuItem.map((item, index) => (
-                    <NavLink to={item.path} keys={index} className="flex text-gray-200 hover:tracking-wide m-2 p-2 gap-4 rounded-xl" activeclassName="active">
-                        <div className=" text-2xl  "> {item.icon} </div>
-                        <div  className={` whitespace-pre duration-500 ${!isOpen && "opacity-0 w-32 px-1 translate-x-20 overflow-hidden "}`}>{item.name}</div>
+                    <NavLink to={item.path} keys={index} className="flex text-gray-200 hover:tracking-wide m-2 p-2 gap-4 rounded-xl">
+                        <div className=" text-2xl"> {item.icon} </div>
+                        <div className="px-2">{item.name}</div>
                     </NavLink>
                 ))
             }
-            <div style={{ display: isOpen ? "block" : "none" }} className=" py-24 m-3 text-center justify-center transition-all duration-500 before:content-[''] before:fixed before:bottom-64 before:left-0 before:translate-x-1/2 before:w-12 before:h-0.5 before:transition-all before:duration-500 before:bg-blue-500 hover:before:w-36 hover:before:opacity-100">
+            <div  className=" m-12 h-7 text-center justify-center bg-gradient-to-tr from-[#0700c5e3] to-[#00e1ffe0] rounded-md hover:bg-gradient-to-bl ">
             {
-                user.map((user, page) => (
-                    <NavLink to={user.way} keys={page} className="flex text-gray-200 rounded-md h-7 gap-3">
-                        <div className=" text-2xl px-4"> {user.icon} </div>
-                        <div  className={` whitespace-pre duration-500 ${!isOpen && " opacity-0  translate-x-20 overflow-hidden "}`}>{user.call}</div>
+                user.map((user, index) => (
+                    <NavLink to={user.path} keys={index} className="text-gray-200  ">
+                        <div>{user.name}</div>
                     </NavLink>
                 ))
             }
             </div>
-            <div style={{ display: isOpen ? "block" : "none" }}>
-                <div className="grid grid-cols-4 mx-auto w-30 h-24 text-3xl gap-5 p-3 m-10 justify-center cursor-pointer">
+            <div>
+                <div className="grid grid-cols-4 h-20 text-3xl gap-5 my-44 m-5 justify-center cursor-pointer">
                     <div className="flex items-top text-blue-600 hover:text-blue-300">
                         <a href='https://www.instagram.com' target="blank" >
                             <RiInstagramFill />
@@ -100,8 +93,8 @@ return (
                 </div>
             </div>
         </div>
-        <main>{children}</main>
     </div>
+    
     );
 };
 
