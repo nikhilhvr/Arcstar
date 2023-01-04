e7ff480c19c6c14bdce4e67fdf135c80acff03c4
Mardav Jadaun <mardavjadaun113@gmail.com>
2023-01-04 20:01:55 +0530

    committed mobile responsive code in homepage.

--- untitled/src/components/sidebar.js
+++ untitled/src/components/sidebar.js
@@ -28,7 +28,7 @@ const Sidebar = () => {
     },
     {
         path: "/about",
-        name: "About Us",
+        name: "AboutUs",
         icon: <HiOutlineInformationCircle/>
     },
     {
@@ -46,46 +46,46 @@ const user=[
 ]
 
 return (
-    <div className="flex w-full h-full fixed">
-        <div className="bg-black text-white m-2 w-72">
-            <div className="flex p-10 ">
+    <div className="flex w-full md:h-full h-[40%] fixed">
+        <div className="bg-black text-white md:m-2 m-1 md:w-72 w-9 md:rounded-none rounded-full">
+            <div className="flex md:p-10 p-2">
                 <GiFlexibleStar size={45}/>
             </div>  
             {
                 menuItem.map((item, index) => (
-                    <NavLink to={item.path} keys={index} className="flex text-gray-200 hover:tracking-wide m-2 p-2 gap-4 rounded-xl">
-                        <div className=" text-2xl"> {item.icon} </div>
-                        <div className="px-2">{item.name}</div>
+                    <NavLink to={item.path} keys={index} className="flex hover:tracking-wide md:m-2 m-1 md:p-2 p-1 md:gap-4 rounded-xl">
+                        <div className="md:text-2xl text-gray-200 text-xl"> {item.icon} </div>
+                        <div className="px-2 md:text-gray-200 overflow-hidden">{item.name}</div>
                     </NavLink>
                 ))
             }
-            <div  className=" m-12 h-7 text-center justify-center bg-gradient-to-tr from-[#0700c5e3] to-[#00e1ffe0] rounded-md hover:bg-gradient-to-bl ">
+            <div  className="m-12 h-7 text-center justify-center bg-gradient-to-tr from-[#0700c5e3] to-[#00e1ffe0] rounded-md hover:bg-gradient-to-bl overflow-hidden">
             {
                 user.map((user, index) => (
-                    <NavLink to={user.path} keys={index} className="text-gray-200  ">
+                    <NavLink to={user.path} keys={index} className="text-gray-200">
                         <div>{user.name}</div>
                     </NavLink>
                 ))
             }
             </div>
             <div>
-                <div className="grid grid-cols-4 h-20 text-3xl gap-5 my-44 m-5 justify-center cursor-pointer">
-                    <div className="flex items-top text-blue-600 hover:text-blue-300">
+                <div className="md:grid grid-cols-4 md:text-3xl text-2xl md:gap-5 py-32 md:m-5 m-1 justify-center items-center md:cursor-pointer text-blue-600">
+                    <div className="flex md:text-blue-600 md:hover:text-blue-300 py-1">
                         <a href='https://www.instagram.com' target="blank" >
                             <RiInstagramFill />
                         </a>
                     </div>
-                    <div className="flex items-center text-blue-500 hover:text-blue-300">
+                    <div className="flex md:text-blue-500 md:hover:text-blue-300 py-1">
                         <a href='https://www.github.com' target="blank" >
                             <RiGithubFill />
                         </a>
                     </div>
-                    <div className="flex items-top text-blue-400 hover:text-blue-500">
+                    <div className="flex md:text-blue-400 md:hover:text-blue-500 py-1">
                         <a href='https://www.linkedin.com' target="blank" >
                             <RiLinkedinBoxFill />
                         </a>
                     </div>
-                    <div className="flex items-center  text-blue-300 hover:text-blue-500">
+                    <div className="flex md:text-blue-300 md:hover:text-blue-500 py-1">
                         <a href='https://www.twitter.com' target="blank" >
                             <RiTwitterFill />
                         </a>
--- untitled/src/webpages/Home.js
+++ untitled/src/webpages/Home.js
@@ -6,27 +6,27 @@ import { NavLink } from 'react-router-dom';
 const Home = () => {
   return (
     <div className="flex ">
-      <section className=" w-3/12"></section>
+      <section className="w-3/12"></section>
       <section className="w-full relative">
-      <video className="h-full w-full fixed" src={Background} autoPlay loop muted />
+      <video className="h-screen w-full fixed" src={Background} autoPlay loop muted />
               <div className="absolute my-10"> 
-        <h1 className="text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-tr from-[#080398e3] via-[#1fe5ff] to-[#00e1ffe0] ml-24 ">ArcStar</h1>
-        <h2 className="text-xl text-black ml-40">-Closer like Proxima Centurai.</h2>
-        <h2 className="m-10 mx-60 w-7/12 text-justify text-gray-500"> A student coding platform for beginners. This platform allows beginner students to embark on their 100 days of code challenge and connect with other students to discuss their progress and exchange ideas. This platform will not only help students stay motivated and accountable during their coding journey, but also provide them with a supportive community of like-minded individuals to turn to for guidance and inspiration at the same time.</h2>
-        <div className="flex w-80 m-16 ml-28 ">
-          <img className="rounded-full" src={Image} />
-            <div className="flex gap-10 m-20 text-white text-xl font-semibold">
+        <h1 className="md:text-5xl text-3xl font-extrabold text-transparent bg-clip-text bg-gradient-to-tr from-[#080398e3] via-[#1fe5ff] to-[#00e1ffe0] md:ml-24 ">ArcStar</h1>
+        <h2 className="md:text-xl text-sm text-black md:ml-40 ml-10">-Closer like Proxima Centurai.</h2>
+        <h2 className="md:m-10 m-5 md:ml-60 md:w-7/12 md:text-justify text-gray-500 md:text-[16px] text-[13px]"> A student coding platform for beginners. This platform allows beginner students to embark on their 100 days of code challenge and connect with other students to discuss their progress and exchange ideas. This platform will not only help students stay motivated and accountable during their coding journey, but also provide them with a supportive community of like-minded individuals to turn to for guidance and inspiration at the same time.</h2>
+        <div className="md:flex md:w-80 md:h-80 w-52 md:m-16 m-3 md:ml-28">
+          <img className="rounded-full justify-center items-center" src={Image} />
+            <div className="flex md:gap-10 gap-3 md:m-20 my-10 text-white text-xl font-semibold w-0 h-0 ">
               <NavLink href="#" target="blank">
-                <div className="bg-gradient-to-tr from-black via-gray-800 to-gray-500 w-52 h-40 justify-center text-center p-5 hover:scale-110 duration-200">
+                <div className="bg-gradient-to-tr from-black via-gray-800 to-gray-500 justify-center md:text-[22px] text-[14px] md:w-52 md:h-40  w-[115px] h-30 text-center md:p-5 p-2 hover:scale-110 duration-200">
                   100DaysofCode Challenge
-                  <h1 className="text-sm text-end py-9 whitespace-nowrap font-extralight hover:tracking-wider duration-300">Get Started !</h1>
+                  <h1 className="md:text-sm text-xs text-end md:py-9 py-2 whitespace-nowrap font-extralight hover:tracking-wider duration-300">Get Started !</h1>
                 </div>
               </NavLink>
 
               <NavLink href="#" target="blank">
-                <div className="bg-gradient-to-tr from-[#080398e3] via-[#0069ff] to-[#00e1ffe0] h-40 w-52 justify-center text-center p-5 hover:scale-110 duration-200">
+                <div className="bg-gradient-to-tr from-[#080398e3] via-[#0069ff] to-[#00e1ffe0] justify-center md:text-[22px] text-[14px] md:w-52 md:h-40 h-30 w-[115px] text-center md:p-5 p-2 hover:scale-110 duration-200">
                   Take the Online Quiz 
-                  <h1 className="text-sm text-end py-10 whitespace-nowrap font-extralight hover:tracking-wider duration-300">Test your skills ! </h1>
+                  <h1 className="md:text-sm text-xs text-end md:py-10 py-2 whitespace-nowrap font-extralight hover:tracking-wider duration-300">Test your skills ! </h1>
                 </div>
               </NavLink>
             </div>
